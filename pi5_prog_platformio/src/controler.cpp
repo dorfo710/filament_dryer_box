901837d51223020e8afd37b45c151719504b8c38
dorfo710 <budirodolfo@gmail.com>
2021-11-15 18:48:06 -0300

    Update controler.cpp

--- pi5_prog_platformio/src/controler.cpp
+++ pi5_prog_platformio/src/controler.cpp
@@ -17,14 +17,28 @@ public:
     bool temp_control_state;
     bool humidity_control_state;
 
-    void Set_temp_pwm_config(int Frequency, int Channel, int Resolution)
-    {
+    int Temp_PWM_FREQUENCY;
+    int Temp_PWM_CHANNEL;
+    int Temp_PWM_RESOLUTION;
 
+    int humidity_PWM_FREQUENCY;
+    int humidity_PWM_CHANNEL;
+    int humidity_PWM_RESOLUTION;
+
+    void Set_temp_pwm_config(int GPIO_pin, int Frequency, int Channel, int Resolution)
+    {
+    humidity_PWM_FREQUENCY = Frequency;
+    humidity_PWM_CHANNEL = Channel;
+    humidity_PWM_RESOLUTION = Resolution;
+    ledcAttachPin(GPIO_pin, Channel);
+    ledcSetup(Channel, Frequency, Resolution);
     }
 
     void Set_hum_pwm_config(int Frequency, int Channel, int Resolution)
     {
-
+    Temp_PWM_FREQUENCY = Frequency;
+    Temp_PWM_CHANNEL = Channel;
+    Temp_PWM_RESOLUTION = Resolution;
     }
 
     void Set_desired_temperature(float DT)
@@ -47,29 +61,33 @@ public:
         
     }
 
-    void Control_temp() // controls the temperature of the sistem
+    void Control_temp(float curr_temp) // controls the temperature of the sistem
     {
         if (temp_control_state==0) // returns if the control temp state is set to 0
         {
         printf("Temperature control state turned OFF");
+        ledcWrite(temp_PWM_CHANNEL, 0); // sets the dutycicle to 0
         return;
         }
         else
         {
-
+            int dummy_DutyCycle = 5;
+            ledcWrite(Temp_PWM_CHANNEL, dummy_DutyCycle);
         }
     }
 
-    void Control_humidity()
+    void Control_humidity(float curr_humidity)
     {
         if (humidity_control_state==0) // returns if the control humidity state is set to 0
         {
         printf("humidity control state turned OFF");
+        ledcWrite(humidity_PWM_CHANNEL, 0); // sets the dutycicle to 0
         return;
         }
         else
         {
-
+            int dummy_DutyCycle = 5;
+            ledcWrite(humidity_PWM_CHANNEL, dummy_DutyCycle);
         }
     }
 
