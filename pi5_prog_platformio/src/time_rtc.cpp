4b1c03229293773c2cfa08fc8957f29c3c1b80eb
dorfo710 <budirodolfo@gmail.com>
2021-11-15 18:35:56 -0300

    Create time_rtc.cpp

---
+++ pi5_prog_platformio/src/time_rtc.cpp
@@ -0,0 +1,15 @@
+
+// Pino ligado ao CS do modulo
+const int chipSelect = 5;
+String Dados;
+int tempoDelay = 6000;
+char tempo[7];
+unsigned int atual = 0;
+char VetorTempo[17];
+
+
+int ano = 0;
+int mes = 0;
+int dia = 0, AuxDia = 0;
+int hora = 0;
+int minuto = 0;
