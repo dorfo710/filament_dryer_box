9c8b404ab58c80984eb56909469f655a34ff37b8
PADELF <pauloafonso2210@gmail.com>
2021-11-17 12:21:05 -0300

    Update time_rtc.cpp

--- pi5_prog_platformio/src/time_rtc.cpp
+++ pi5_prog_platformio/src/time_rtc.cpp
@@ -1,15 +1,21 @@
+#include <iostream>
+#include "time_rtc.h"
+using namespace std;
 
-// Pino ligado ao CS do modulo
-const int chipSelect = 5;
-String Dados;
-int tempoDelay = 6000;
-char tempo[7];
-unsigned int atual = 0;
-char VetorTempo[17];
+int DiaSemana, Porta, TempoAcao, H0, H1 , H2, H3, H4, H5, H6, H7, H8, H9, H10, H11, H12, H13, H14, H15, H16, H17, H18, H19, H20, H21, H22, H23, tempo;
+bool Auxiliar = false, auxiliar2 = false, Aux2 =  true;
 
+Hora::Hora(){	
+  
+}
 
-int ano = 0;
-int mes = 0;
-int dia = 0, AuxDia = 0;
-int hora = 0;
-int minuto = 0;
+void Hora::Definir(int hora, int minuto, int dia, int mes, int ano) {    
+    rtc.adjust(DateTime(ano, mes, dia, hora, minuto, 0));
+
+}
+char Hora::Atual(){
+    DateTime now = rtc.now();
+    String Dados = String(now.day()) + "/" + String(now.month()) + "/" + String(now.year()) + " " + String(now.hour()) + ":"
+    + String(now.minute()) + ":" + String(now.second());
+    return Dados;
+}
