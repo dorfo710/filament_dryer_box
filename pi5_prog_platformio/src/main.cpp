4af7ae8c4f3f044af9de0fd995976178009538bd
PADELF <pauloafonso2210@gmail.com>
2021-11-23 11:41:50 -0300

    Update main.cpp

--- pi5_prog_platformio/src/main.cpp
+++ pi5_prog_platformio/src/main.cpp
@@ -1,8 +1,11 @@
+//Bibliotecas
 #include "SdFat.h"
 #include <EEPROM.h>
 #include <Wire.h>
 #include "RTClib.h"
+#include <DHT.h>
 
+//Funções
 #include "time_rtc.h"
 #include "controler.cpp"
 #include "Leitura.h"
@@ -12,29 +15,31 @@ SdFile meuArquivo;
 RTC_DS3231 rtc;
 
 
-//#comentario laura
-#include <DHT.h> //INCLUSÃO DE BIBLIOTECA
 
 #define DHTPIN 15 //PINO DIGITAL UTILIZADO PELO DHT22
 #define DHTTYPE DHT22 //DEFINE O MODELO DO SENSOR (DHT22 / AM2302)
 
 #define DHTPIN2 14 
 
-DHT dht(DHTPIN, DHTTYPE); //PASSA OS PARÂMETROS PARA A FUNÇÃO
+DHT dht1(DHTPIN, DHTTYPE); //PASSA OS PARÂMETROS PARA A FUNÇÃO
 DHT dht2(DHTPIN2, DHTTYPE);
 
 // Pino ligado ao CS do modulo
 const int chipSelect = 5;
 void setup()
 {
-
+  Serial.begin(115200); //INICIALIZA A SERIAL
+  EEPROM.begin(10);
+  Wire.begin(33, 32);
+  if (! rtc.begin()) {
+    Serial.println("Não foi possível encontrar RTC");
+    while (1);
+  }
+  dht1.begin();
+  dht2.begin();
 }
 
 void loop()
 {
-  if (analogRead(13) > 600)
-  {
-    while (1) {}
-  }
-  }
+  
 }
