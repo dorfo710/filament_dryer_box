ad11fca82d168ff88aaec7245dbbdf4346210bac
PADELF <pauloafonso2210@gmail.com>
2021-11-23 22:45:08 -0300

    Update main.cpp

--- pi5_prog_platformio/src/main.cpp
+++ pi5_prog_platformio/src/main.cpp
@@ -4,34 +4,45 @@
 #include <Wire.h>
 #include "RTClib.h"
 #include <DHT.h>
+#include <Nextion.h>
 
 //Funções
 #include "time_rtc.h"
 #include "controler.cpp"
 #include "Leitura.h"
+#include "Interface.h"
 
 SdFat sdCard;
 SdFile meuArquivo;
 RTC_DS3231 rtc;
+//Interface Interface;
+
+//Nextion
+NexButton b0 = NexButton(0, 26, "b14");
+NexTouch *nex_listen_list[] =
+{
+  &b0,
+  NULL  // String terminated
+};
 
 
-
-#define DHTPIN 15 //PINO DIGITAL UTILIZADO PELO DHT22
+#define DHTPIN 15     //PINO DIGITAL UTILIZADO PELO DHT22
 #define DHTTYPE DHT22 //DEFINE O MODELO DO SENSOR (DHT22 / AM2302)
 
-#define DHTPIN2 14 
+#define DHTPIN2 14
 
 DHT dht1(DHTPIN, DHTTYPE); //PASSA OS PARÂMETROS PARA A FUNÇÃO
 DHT dht2(DHTPIN2, DHTTYPE);
 
-// Pino ligado ao CS do modulo
-const int chipSelect = 5;
+
 void setup()
 {
+  
   Serial.begin(115200); //INICIALIZA A SERIAL
   EEPROM.begin(10);
   Wire.begin(33, 32);
-  if (! rtc.begin()) {
+  if (!rtc.begin())
+  {
     Serial.println("Não foi possível encontrar RTC");
     while (1);
   }
@@ -41,5 +52,11 @@ void setup()
 
 void loop()
 {
+  nexLoop(nex_listen_list);
   
+}
+
+void b0PopCallback(void *ptr)  
+{
+ Interface.NexRtcDefinir();
 }
