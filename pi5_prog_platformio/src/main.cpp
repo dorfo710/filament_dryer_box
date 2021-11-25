49e676977a0186cda8bbf6ebe4e7945333dcc8c0
PADELF <pauloafonso2210@gmail.com>
2021-11-24 23:32:52 -0300

    Update main.cpp

--- pi5_prog_platformio/src/main.cpp
+++ pi5_prog_platformio/src/main.cpp
@@ -1,82 +1,52 @@
 //Bibliotecas
-#include "SdFat.h"
-#include <EEPROM.h>
 #include <Wire.h>
-#include "RTClib.h"
-#include <DHT.h>
-#include <Nextion.h>
+#include "Nextion.h"
 
 //Funções
 #include "time_rtc.h"
-#include "controler.cpp"
+//#include "controler.cpp"
 #include "Leitura.h"
 #include "Interface.h"
 
-SdFat sdCard;
-SdFile meuArquivo;
-RTC_DS3231 rtc;
+uint32_t ds_var;
 Interface Interface;
+Hora Hora;
 
 //Nextion
 NexButton b0 = NexButton(0, 26, "b14");
+NexButton b1 = NexButton(0, 28, "b15");
+
+int HORA, MINUTO, DIA, MES, ANO;
+void setup()
+{
+  nexInit();
+  Wire.begin(33, 32);
+  b0.attachPop(b0PopCallback, &b0);
+  b1.attachPop(b1PopCallback, &b1);
+}
+
 NexTouch *nex_listen_list[] =
 {
   &b0,
+  &b1,
   NULL  // String terminated
 };
 
 
-#define DHTPIN 15     //PINO DIGITAL UTILIZADO PELO DHT22
-#define DHTTYPE DHT22 //DEFINE O MODELO DO SENSOR (DHT22 / AM2302)
-
-#define DHTPIN2 14
-
-DHT dht1(DHTPIN, DHTTYPE); //PASSA OS PARÂMETROS PARA A FUNÇÃO
-DHT dht2(DHTPIN2, DHTTYPE);
-
-double Temperatura;
-double Umidade;
-int Resistor; 
-int Piezo; 
-int Vento; 
-int Vento2; 
-
-void setup()
-{
-  
-  Serial.begin(115200); //INICIALIZA A SERIAL
-  EEPROM.begin(10);
-  Wire.begin(33, 32);
-  if (!rtc.begin())
-  {
-    Serial.println("Não foi possível encontrar RTC");
-    while (1);
-  }
-  dht1.begin();
-  dht2.begin();
-}
-
 void loop()
 {
   nexLoop(nex_listen_list);
-  
+
 }
 
-void b0PopCallback(void *ptr)  
+void b0PopCallback(void *ptr)
 {
- Interface.NexRtcDefinir();
+  Hora.Ajustar(Interface.NexRtcDefinirAno(), Interface.NexRtcDefinirMes(), Interface.NexRtcDefinirDia(),
+               Interface.NexRtcDefinirHora(), Interface.NexRtcDefinirMinuto());
+  Interface.NexRtcPrint(Hora.Atual());
 }
 
-//TESTE LEITURA
-void leitura(){
-  Leitura T, U, R, P, V, V2, LSD;
-
-  Temperatura = T.getTemp();
-  Umidade = U.getUmid();
-  Resistor = R.getResist();
-  Piezo = P.getPiezo();
-  Vento = V.getVent1();
-  Vento2 =  V2.getVent2();
-  LSD.SdCard();
-
+void b1PopCallback(void *ptr)
+{
+  Interface.NexRtcPrint(Hora.Atual());
 }
