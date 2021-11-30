d17afe956a5666b58f923adbcf026279caa3aa31
PADELF <pauloafonso2210@gmail.com>
2021-11-30 12:03:26 -0300

    Update time_rtc.cpp

--- pi5_prog_platformio/src/time_rtc.cpp
+++ pi5_prog_platformio/src/time_rtc.cpp
@@ -25,6 +25,7 @@ String Hora::Atual()
 
 bool Hora::Timer()
 {
+    DateTime now = rtc.now();
     if (now.second() == 0 || now.second() == 20 || now.second() == 40 || now.second() == 60)
     {
         return true;
