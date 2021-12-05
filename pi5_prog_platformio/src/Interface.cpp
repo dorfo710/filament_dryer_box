49ad6e23fa5d4c6dfaa60e12325175b133322de7
GusBlack <93201656+GusBlack@users.noreply.github.com>
2021-12-05 10:50:54 -0300

    Update Interface.cpp

--- pi5_prog_platformio/src/Interface.cpp
+++ pi5_prog_platformio/src/Interface.cpp
@@ -9,13 +9,13 @@ char buffer[25];
 
 
 // === Declaração de Objetos ===
-/*Nexsettemp h0 = Nexsettemp(0, 5, "h0"); // Setup das funções do LCD
+  Nexsettemp h0 = Nexsettemp(0, 5, "h0"); // Setup das funções do LCD
   Nex30umid c0 = Nex30umid(0, 13, "c0");
   Nex50umid c1 = Nex50umid(0, 14, "c1");
   Nex70umid c2 = Nex70umid(0, 15, "c2");
   Nextempautal t1 = Nextempautal(0, 3, "t1");
   NexUmidatual t4 = NexUmidatual(0, 8, "t4");
-*/
+
 // RTC Ajuste @@@Definir pagina@@@
 NexText v1 = NexText(0, 1, "t0");
 NexText v2 = NexText(0, 6, "t6");
