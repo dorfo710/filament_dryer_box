5984f0e3b083e96f22b6c4d0a899410693ba250a
PADELF <pauloafonso2210@gmail.com>
2021-12-07 16:57:31 -0300

    Update Interface.cpp

--- pi5_prog_platformio/src/Interface.cpp
+++ pi5_prog_platformio/src/Interface.cpp
@@ -7,14 +7,13 @@
 using namespace std;
 char buffer[25];
 
-
 // === Declaração de Objetos ===
-  Nexsettemp h0 = Nexsettemp(0, 5, "h0"); // Setup das funções do LCD
-  Nex30umid c0 = Nex30umid(0, 13, "c0");
-  Nex50umid c1 = Nex50umid(0, 14, "c1");
-  Nex70umid c2 = Nex70umid(0, 15, "c2");
-  Nextempautal t1 = Nextempautal(0, 3, "t1");
-  NexUmidatual t4 = Nexumidatual(0, 8, "t4");
+NexSlider h0 = NexSlider(0, 5, "h0"); // Setup das funções do LCD
+NexCheckbox c0 = NexCheckbox(0, 13, "c0");
+NexCheckbox c1 = NexCheckbox(0, 14, "c1");
+NexCheckbox c2 = NexCheckbox(0, 15, "c2");
+NexText t1 = NexText(0, 3, "t1");
+NexText t4 = NexText(0, 8, "t4");
 
 // RTC Ajuste @@@Definir pagina@@@
 NexText v1 = NexText(0, 1, "t0");
@@ -23,7 +22,7 @@ NexText v3 = NexText(0, 7, "t7");
 NexText v4 = NexText(0, 8, "t8");
 NexText v5 = NexText(0, 9, "t9");
 
-NexText v6   = NexText(0, 27, "t10"); // Ajustar
+NexText v6 = NexText(0, 27, "t10"); // Ajustar
 
 Interface::Interface()
 {
@@ -36,7 +35,8 @@ int Interface::NexRtcDefinirDia()
   return DIA;
 }
 
-int Interface::NexRtcDefinirMes() {
+int Interface::NexRtcDefinirMes()
+{
 
   memset(buffer, 0, sizeof(buffer));
   v4.getText(buffer, sizeof(buffer));
@@ -44,7 +44,8 @@ int Interface::NexRtcDefinirMes() {
   return MES;
 }
 
-int Interface::NexRtcDefinirAno() {
+int Interface::NexRtcDefinirAno()
+{
 
   memset(buffer, 0, sizeof(buffer));
   v5.getText(buffer, sizeof(buffer));
@@ -52,53 +53,48 @@ int Interface::NexRtcDefinirAno() {
   return ANO;
 }
 
-int Interface::NexRtcDefinirHora() {
+int Interface::NexRtcDefinirHora()
+{
   memset(buffer, 0, sizeof(buffer));
   v1.getText(buffer, sizeof(buffer));
   HORA = atoi(buffer);
 
   return HORA;
 }
-int Interface::NexRtcDefinirMinuto() {
-  
+int Interface::NexRtcDefinirMinuto()
+{
+
   memset(buffer, 0, sizeof(buffer));
   v2.getText(buffer, sizeof(buffer));
   MINUTO = atoi(buffer);
   return MINUTO;
 }
 
-
-void Interface::NexRtcPrint(String Valor, String Local)
+void Interface::NexPrint(String Valor, String Local)
 {
   char conversao_S_C[30];
   Valor.toCharArray(conversao_S_C, 30);
 
-  if(Local=="Hora"){
-    v6.setText(conversao_S_C);   
+  if (Local == "Hora")
+  {
+    v6.setText(conversao_S_C);
   }
-  
 }
 void Interface::Nexsettemp()
 {
-
 }
 void Interface::Nextempautal()
 {
-
 }
 void Interface::NexUmidatual()
 {
-
 }
 void Interface::Nex30umid()
 {
-
 }
 void Interface::Nex50umid()
 {
-
 }
 void Interface::Nex70umid()
 {
-
 }
