049621ef3e670f503a2d55414be5e6c6b6c805e9
PADELF <pauloafonso2210@gmail.com>
2021-12-08 07:50:10 -0300

    Update Interface.cpp

--- pi5_prog_platformio/src/Interface.cpp
+++ pi5_prog_platformio/src/Interface.cpp
@@ -1,28 +1,34 @@
 #include <Nextion.h>   // Biblioteca lcd
 #include "Interface.h" // Biblioteca dos macro da interface
-#include "Leitura.h"   // Biblioteca de leitura
-#include "time_rtc.h"
 #include <iostream>
 
 using namespace std;
 char buffer[25];
 
 // === Declaração de Objetos ===
+//Page0
 NexSlider h0 = NexSlider(0, 5, "h0"); // Setup das funções do LCD
 NexCheckbox c0 = NexCheckbox(0, 13, "c0");
 NexCheckbox c1 = NexCheckbox(0, 14, "c1");
 NexCheckbox c2 = NexCheckbox(0, 15, "c2");
-NexText t1 = NexText(0, 3, "t1");
-NexText t4 = NexText(0, 8, "t4");
+NexText TempP0 = NexText(0, 2, "t1");
+NexText UmidP0 = NexText(0, 7, "t4");
+NexText MostradorP0 = NexText(0, 18, "t10");
+//Page1
+NexText MostradorP1 = NexText(1, 4, "t10");
 
-// RTC Ajuste @@@Definir pagina@@@
+//Page2
+NexText TempP2 = NexText(2, 4, "t2");
+NexText UmidP2 = NexText(2, 5, "t3");
+NexText MostradorP2 = NexText(2, 3, "t10");
+
+//Page3
 NexText v1 = NexText(0, 1, "t0");
 NexText v2 = NexText(0, 6, "t6");
 NexText v3 = NexText(0, 7, "t7");
 NexText v4 = NexText(0, 8, "t8");
 NexText v5 = NexText(0, 9, "t9");
-
-NexText v6 = NexText(0, 27, "t10"); // Ajustar
+NexText MostradorP3 = NexText(0, 27, "t10"); 
 
 Interface::Interface()
 {
@@ -75,11 +81,39 @@ void Interface::NexPrint(String Valor, String Local)
   char conversao_S_C[30];
   Valor.toCharArray(conversao_S_C, 30);
 
+  if (Local == "Aviso")
+  {
+    MostradorP0.setText(conversao_S_C);
+    MostradorP1.setText(conversao_S_C);
+    MostradorP2.setText(conversao_S_C);
+    MostradorP3.setText(conversao_S_C);
+  }
   if (Local == "Hora")
   {
-    v6.setText(conversao_S_C);
+    MostradorP0.setText(conversao_S_C);
+    MostradorP1.setText(conversao_S_C);
+    MostradorP2.setText(conversao_S_C);
+    MostradorP3.setText(conversao_S_C);
   }
+  if (Local == "Temp")
+  {
+    TempP0.setText(conversao_S_C);
+    TempP2.setText(conversao_S_C);
+  }
+  if (Local == "Umid")
+  {
+    UmidP0.setText(conversao_S_C);
+    UmidP2.setText(conversao_S_C);
+  }
+
+
+
+
+
 }
+
+
+
 void Interface::Nexsettemp()
 {
 }
