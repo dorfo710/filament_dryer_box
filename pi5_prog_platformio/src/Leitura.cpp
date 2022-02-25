645c1d8dd028c9ed6c0cd8f57da57682d6ae47de
dorfo710 <budirodolfo@gmail.com>
2022-02-25 17:53:42 -0300

    Update Leitura.cpp

--- pi5_prog_platformio/src/Leitura.cpp
+++ pi5_prog_platformio/src/Leitura.cpp
@@ -1,4 +1,4 @@
-//Só exemplos para começar :)
+// Só exemplos para começar :)
 
 #include <iostream>
 #include "Leitura.h"
@@ -15,21 +15,19 @@ using namespace std;
 #define Vent1_port 26
 #define Vent2_port 27
 
-#define DHTPIN 13     //PINO DIGITAL UTILIZADO PELO DHT22
+#define DHTPIN 13 // PINO DIGITAL UTILIZADO PELO DHT22
 #define DHTPIN2 14
-#define DHTTYPE DHT22 //DEFINE O MODELO DO SENSOR (DHT22 / AM2302)
+#define DHTTYPE DHT22 // DEFINE O MODELO DO SENSOR (DHT22 / AM2302)
 
-
-DHT dht1(DHTPIN, DHTTYPE); //PASSA OS PARÂMETROS PARA A FUNÇÃO
+DHT dht1(DHTPIN, DHTTYPE); // PASSA OS PARÂMETROS PARA A FUNÇÃO
 DHT dht2(DHTPIN2, DHTTYPE);
 
 SdFat sdCard;
 SdFile meuArquivo;
 const int chipSelect = 5;
 
-
-//Construtor padrão não recebe nenhum parâmetro
-//Este construtor sempre está presente mesmo que não tenha sido Declarado
+// Construtor padrão não recebe nenhum parâmetro
+// Este construtor sempre está presente mesmo que não tenha sido Declarado
 Leitura::Leitura()
 {
     Temp = 0;
@@ -52,7 +50,7 @@ Leitura::Leitura(double T, double U, int R, int P, int V)
     cout << "Construindo(3) um objeto da classe Circle!" << endl;
 }
 */
-//Destrutor
+// Destrutor
 Leitura::~Leitura()
 {
     Temp = 0;
@@ -60,42 +58,32 @@ Leitura::~Leitura()
     Resist = 0;
     Piezo = 0;
     Vent = 0;
-    //Imprime o texto indicado que o objeto está sendo destruido
+    // Imprime o texto indicado que o objeto está sendo destruido
     cout << "Destruindo o objeto da classe Ci l rc e!" << endl;
 }
 
-//Métodos Acessores
-double Leitura::getTemp()
-{ //Recupera temperatura media
+// Métodos Acessores
+float Leitura::getTemp()
+{ // Recupera temperatura media
     int temperatura1 = dht1.readTemperature();
-    int temperatura2 = dht2.readTemperature();
+    int temperatura2 = dht1.readTemperature();
     Temp = (temperatura1 + temperatura2) / 2;
     return Temp;
 }
-double Leitura::getUmid()
-{ //Recupera umidade media
+float Leitura::getUmid()
+{ // Recupera umidade media
     int umidade1 = dht1.readHumidity();
-    int umidade2 = dht2.readHumidity();
+    int umidade2 = dht1.readHumidity();
     Umid = (umidade1 + umidade2) / 2;
     return Umid;
 }
-int Leitura::getResist()
-{ //Recupera o estado da Resistência
-    Resist = digitalRead(Resistencia_Port);
-    return Resist;
-}
-int Leitura::getPiezo()
-{ //Recupera o valor do Piezo
-    Piezo = digitalRead(Piezo_Port);
-    return Piezo;
-}
 int Leitura::getVent1()
-{ //Recupera o valor da ventoinha 1
+{ // Recupera o valor da ventoinha 1
     Vent = digitalRead(Vent1_port);
     return Vent;
 }
 int Leitura::getVent2()
-{ //Recupera o valor da ventoinha 2
+{ // Recupera o valor da ventoinha 2
     Vent2 = digitalRead(Vent2_port);
     return Vent2;
 }
@@ -113,7 +101,7 @@ void Leitura::SdCard(String Hora, String UmiD, String TemP)
     String Dados = String(Hora + ";" + UmiD + ";" + TemP + ";");
     meuArquivo.println(Dados);
 
-    //Encerra Cartão
+    // Encerra Cartão
     Serial.println("(˘͜ʖ˘) Processo de gravacao interrompido. Retire o SD! ٩(^ᴗ^)۶");
     meuArquivo.close();
 }
