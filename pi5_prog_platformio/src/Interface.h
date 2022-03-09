12b766e211344231520a3c38a15226be889266de
dorfo710 <budirodolfo@gmail.com>
2022-03-09 10:51:12 -0300

    Update Interface.h

--- pi5_prog_platformio/src/Interface.h
+++ pi5_prog_platformio/src/Interface.h
@@ -17,7 +17,8 @@ public:
   int NexRtcDefinirMes();
   int NexRtcDefinirAno();
   void NexPrint(String, String);
-  
+  int NexGetInt(String);
+
   void NexUmidatual();
   void Nextempautal();
   void Nexsettemp();
