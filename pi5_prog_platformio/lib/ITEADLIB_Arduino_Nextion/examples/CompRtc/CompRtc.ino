32b2395ddb359b7c04ce9afa348c499427ac2217
dorfo710 <budirodolfo@gmail.com>
2022-03-09 10:50:48 -0300

    Update CompRtc.ino

--- pi5_prog_platformio/lib/ITEADLIB_Arduino_Nextion/examples/CompRtc/CompRtc.ino
+++ pi5_prog_platformio/lib/ITEADLIB_Arduino_Nextion/examples/CompRtc/CompRtc.ino
@@ -2,12 +2,12 @@
  * @example CompRtc.ino
  *
  * @par How to Use
- * This example shows that in nextion screen 
+ * This example shows that in nextion screen
  * displays the current read the RTC time and show how to use the API.
  *
  * @author  huangxiaoming (email:<xiaoming.huang@itead.cc>)
  * @date    2016/12/8
- * @copyright 
+ * @copyright
  * Copyright (C) 2014-2015 ITEAD Intelligent Systems Co., Ltd. \n
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -17,20 +17,20 @@
 
 #include "Nextion.h"
 
-NexText t0 = NexText(0,1,"t0");
-NexRtc  rtc;
-uint32_t  time[7] = {2016,11,25,12,34,50};
+NexText t0 = NexText(0, 1, "t0");
+NexRtc rtc;
+uint32_t time[7] = {2016, 11, 25, 12, 34, 50};
 uint8_t time_buf[30] = {0};
 
-void setup() 
+void setup()
 {
-    nexSerial.begin(115200);
+    nexSerial.begin(9600);
     rtc.write_rtc_time(time);
 }
 
-void loop() 
+void loop()
 {
-    rtc.read_rtc_time(time_buf,30);
+    rtc.read_rtc_time(time_buf, 30);
     t0.setText(time_buf);
     delay(1000);
 }
