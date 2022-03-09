ddbb4bc1f86156d4a8cd398dd8374db4e57cd94a
dorfo710 <budirodolfo@gmail.com>
2022-03-09 10:50:41 -0300

    Create time_rtc.h

---
+++ PI 5 - Funcionando/time_rtc.h
@@ -0,0 +1,16 @@
+#ifndef _time_rtc_H_
+#define _time_rtc_H_
+
+#include <Arduino.h>
+
+class Hora
+{
+
+private:
+public:
+  Hora();
+  String Atual();
+  void Ajustar(int, int, int, int, int);
+  bool Timer();
+};
+#endif
