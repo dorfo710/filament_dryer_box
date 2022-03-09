16f4f6df659a553e3f053460af3713cb1953368c
dorfo710 <budirodolfo@gmail.com>
2022-03-09 10:50:25 -0300

    Create Interface.h

---
+++ PI 5 - Funcionando/Interface.h
@@ -0,0 +1,30 @@
+#ifndef _Interface_H_
+#define _Interface_H_
+
+#include <Arduino.h>
+
+class Interface
+{
+
+private:
+public:
+  int HORA, MINUTO, DIA, MES, ANO;
+  int SETTEMP, TEMP, UMID;
+  Interface();
+  int NexRtcDefinirMinuto();
+  int NexRtcDefinirHora();
+  int NexRtcDefinirDia();
+  int NexRtcDefinirMes();
+  int NexRtcDefinirAno();
+  void NexPrint(String, String);
+  int NexGetInt(String);
+
+  void NexUmidatual();
+  void Nextempautal();
+  void Nexsettemp();
+  void Nex30umid();
+  void Nex50umid();
+  void Nex70umid();
+  // casa
+};
+#endif
