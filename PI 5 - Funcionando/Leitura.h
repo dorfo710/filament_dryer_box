67b86d97939db22fe676ecc1ae463f237da5dd2f
dorfo710 <budirodolfo@gmail.com>
2022-03-09 10:50:32 -0300

    Create Leitura.h

---
+++ PI 5 - Funcionando/Leitura.h
@@ -0,0 +1,27 @@
+//Só exemplos para começar :)
+
+#ifndef _Leitura_H_
+#define _Leitura_H_
+
+#include <Arduino.h>
+
+class Leitura
+{
+private: // As variáveis membro são privadas (encapsulamento)
+    double Temp, Umid;
+    int Resist, Piezo, Vent, Vent2;
+
+public:        // Os métodos sao públicos
+    Leitura(); //Construtores (sobrecarregado)
+    //Leitura(double, int);
+    //Leitura(double, double, int, int, int, int);
+    ~Leitura();           // Método Destrutor
+    double getTemp(); //Métodos Acessores getXXX
+    double getUmid();
+    int getResist();
+    int getPiezo();
+    int getVent1();
+    int getVent2();
+    void SdCard(String, String, String);
+};
+#endif
